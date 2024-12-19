d8e1d20f0e5bd8e54f56ef2e32c79e10950a94b4
Nisha Rani <8686@saskpolytch.com>
2024-12-19 15:27:45 -0600

    Set up document, run starter code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App header">
+        <h1>Welcome to My abit Tracker App</h1>
+        <p>Start tracking your habits today!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
