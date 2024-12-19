194744f5a5a298d28de433b42508afebe3a490c6
Nisha Rani <8686@saskpolytch.com>
2024-12-19 16:40:56 -0600

    Added delete and completed interactivity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-
-
 import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   const [habits, setHabits] = useState([]); 
 
-  const addHabit = (habit) => {
-    setHabits([...habits, habit]);
+  const addHabit = (habitText) => {
+    setHabits([...habits, 
+      { text: habitText, completed: false }
+    ]);
   };
+
+  const toggleHabit = (index) => {
+    setHabits(
+      habits.map((habit, i) =>
+        i === index ? { ...habit, completed: !habit.completed } : habit)
+    );
+  };
+
+  const deleteHabit = (index) => {
+    setHabits(habits.filter((_, i) => i !== index));
+  }
 
   return (
     <div className="App">
@@ -33,7 +43,18 @@ function App() {
 
       <ul>
         {habits.map((habit, index) => (
-          <li key={index}>{habit}</li>
+          <li key={index}
+          onClick={() => toggleHabit(index)}
+          style={{ textDecoration: habit.completed ? 'line-through' : 'none'}}
+          >
+            {habit.text}
+            <button onClick={(e) => {
+              e.stopPropagation();
+              deleteHabit(index);
+            }}>
+              Delete
+              </button>
+            </li>
         ))}
       </ul>
     </div>
